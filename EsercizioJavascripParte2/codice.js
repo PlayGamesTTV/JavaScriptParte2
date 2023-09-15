39b65391ffd9994ee000c7fec3a4d7c417bf1680
PlayGamesTTV <spade.nasa@gmail.com>
2023-09-15 11:48:47 +0200

    JavaScript Part 2
    Exercises and explanation in javascript Part 2

---
+++ EsercizioJavascripParte2/codice.js
@@ -0,0 +1,17 @@
+function init(){
+	
+    document.getElementById("mioID").innerHTML="testo dal codice";
+    
+    var primoNumero ="4";
+
+    var secondoNumero=5;
+
+    var concatenazione = primoNumero+secondoNumero;
+
+    primoNumero=4;
+
+    var somma = primoNumero+secondoNumero;
+	document.getElementById("mioID").innerHTML="concatenazione= " + concatenazione + "somma=" + somma;
+
+}
+
